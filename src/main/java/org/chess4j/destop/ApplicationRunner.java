c220d9349b0d1798f7e06f5ae3152720f46c1ec3
Andreas <andreas.goss1994@gmail.com>
2022-10-13 18:39:57 +0200

    Setup Hello World

---
+++ src/main/java/org/chess4j/destop/ApplicationRunner.java
@@ -0,0 +1,11 @@
+package org.chess4j.destop;
+
+import javafx.application.Application;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        Application.launch(Chess4jDestop.class, args);
+    }
+
+}
---
+++ src/main/java/org/chess4j/destop/Chess4jDestop.java
@@ -0,0 +1,31 @@
+package org.chess4j.destop;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Chess4jDestop extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
